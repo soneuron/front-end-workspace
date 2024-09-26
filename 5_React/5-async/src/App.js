cdec3fa365765cfb66e18c83bf61c6851d7c12b7
soneuron <ardor00@naver.com>
2024-09-26 15:24:50 +0900

    React

---
+++ 5_React/5-async/src/App.js
@@ -0,0 +1,35 @@
+import { getFetchThen, getAwait } from "./api/movie";
+import { useEffect, useState } from "react";
+
+const App = () => {
+  const [data, setData] = useState("");
+
+  const loadFetchThen = () => {
+    getFetchThen().then((result) => {
+      setData(result);
+      console.log(result);
+    });
+  };
+
+  const loadAwait = async () => {
+    const result = await getAwait();
+    setData(result);
+  };
+
+  useEffect(() => {
+    //loadFetchThen();
+    loadAwait();
+  }, []);
+  return (
+    <>
+      {data.map((movie) => (
+        <div key={movie.id}>
+          <h1>{movie.title}</h1>
+          <h2>{movie.genre}</h2>
+          <p>{movie.actor}</p>
+        </div>
+      ))}
+    </>
+  );
+};
+export default App;
---
+++ 5_React/5-async/src/api/movie.js
@@ -0,0 +1,23 @@
+import axios from "axios";
+// fetch ~ then
+// fetch : 요청을  Promise 기반으로 처리하는 API
+// then : 요청이 끝나고 응답받을 시 데이터 처리
+export const getFetchThen = () => {
+  return fetch("http://localhost:8080/api/movie").then((response) => {
+    return response.json();
+  });
+};
+
+// async ~ await
+// Promise 기반의 비동기 작업을 동기적 코드처럼 작성 할 수 있게 해주는 문법
+export const getAwait = async () => {
+  const response = await fetch("http://localhost:8080/api/movie");
+  return response.json();
+};
+
+// axios(async ~ await)
+// axios는 Promise 기반 HTTP 클라이언트로, fetch보다 간결하고 많은 기능 제공
+// JSON 데이터를 자동으로 파싱
+export const getAxios = async () => {
+  const response = await axios.get();
+};
